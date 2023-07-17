33251447f3874b554ad9fc49f7cb4a693650db09
abhishekdubey1412 <abhishekdubey1412@gmail.com>
2023-07-18 03:51:21 +0530

    create python environment and requirements.txt file

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\abhid\Desktop\New File\venv
+set VIRTUAL_ENV=C:\Users\abhid\Desktop\Python Projects\Converter\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
